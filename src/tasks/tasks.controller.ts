af3dc14b07064808a301841a3de62562db57ed6d
chiraphon-w <chw242542@gmail.com>
2021-05-06 14:39:49 +0700

    sec2-17 interface

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
+import { Task } from './tasks.model';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
-  
+
   @Get()
-  getAllTasks() {
+  getAllTasks():Task[] {
     return this.tasksService.getAllTasks();
   }
 }
